4ac1ca9a323bc7c57c62b1690e335b48a43c7b63
Christoffer Berg <christofferberg79@gmail.com>
2021-12-04 11:22:26 +0100

    Refactored Day1Test

--- src/test/kotlin/Day1Test.kt
+++ src/test/kotlin/Day1Test.kt
@@ -4,31 +4,30 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class Day1Test {
+    private val exampleInput = listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
+    private val realInput = readIntLines("day1.txt")
+
     @Test
-    fun part1_Example1() {
-        val input = listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
-        val result = Day1().part1(input)
+    fun part1_Example() {
+        val result = Day1().part1(exampleInput)
         assertEquals(7, result)
     }
 
     @Test
     fun part1_RealInput() {
-        val input = readIntLines("day1.txt")
-        val result = Day1().part1(input)
+        val result = Day1().part1(realInput)
         assertEquals(1602, result)
     }
 
     @Test
-    fun part2_Example1() {
-        val input = listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
-        val result = Day1().part2(input)
+    fun part2_Example() {
+        val result = Day1().part2(exampleInput)
         assertEquals(5, result)
     }
 
     @Test
     fun part2_RealInput() {
-        val input = readIntLines("day1.txt")
-        val result = Day1().part2(input)
+        val result = Day1().part2(realInput)
         assertEquals(1633, result)
     }
 }
