594f5be6ec67fcbd2876d472d37116a68f0cccdb
Christoffer Berg <christofferberg79@gmail.com>
2021-12-05 10:42:02 +0100

    Refactored Day3

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -3,53 +3,50 @@ package cberg.aoc2021
 class Day3(private val report: List<String>) {
     constructor() : this(Input("day3.txt").lines())
 
+    init {
+        require(report.isNotEmpty())
+        require(report.all { line -> line.length == report.first().length })
+        require(report.all { line -> line.all { char -> char in setOf('0', '1') } })
+    }
+
+    private val indices = report.first().indices
+
     fun part1(): Int {
-        val count = report.first().indices.map { i ->
-            report.map { line -> line[i] }.groupingBy { char -> char }.eachCount()
-        }
-
-        val gamma = count.map { it.maxByOrNull { (_, count) -> count }?.key ?: error("invalid input") }
-            .joinToString(separator = "").toInt(2)
-
-        val epsilon = count.map { it.minByOrNull { (_, count) -> count }?.key ?: error("invalid input") }
-            .joinToString(separator = "").toInt(2)
-
+        val gamma = part1(::gammaSelector)
+        val epsilon = part1(::epsilonSelector)
         return gamma * epsilon
     }
 
+    private fun part1(selector: (List<Char>) -> Char) =
+        indices.map { index -> selectChar(report, index, selector) }
+            .joinToString(separator = "").toInt(2)
+
+    private fun gammaSelector(chars: List<Char>) = chars.last()
+    private fun epsilonSelector(chars: List<Char>) = chars.first()
+
     fun part2(): Int {
-        val indices = report.first().indices
-
-        var work = report
-        for (i in indices) {
-            val count = indices.map { i ->
-                work.map { line -> line[i] }.groupingBy { char -> char }.eachCount()
-            }
-            val ones = count[i].getOrDefault('1', 0)
-            val zeros = count[i].getOrDefault('0', 0)
-            val filterValue = if (ones >= zeros) '1' else '0'
-            work = work.filter { line -> line[i] == filterValue }
-            if (work.size == 1) {
-                break
-            }
-        }
-        val ogr = work.first().toInt(2)
-
-        work = report
-        for (i in indices) {
-            val count = indices.map { i ->
-                work.map { line -> line[i] }.groupingBy { char -> char }.eachCount()
-            }
-            val ones = count[i].getOrDefault('1', 0)
-            val zeros = count[i].getOrDefault('0', 0)
-            val filterValue = if (ones >= zeros) '0' else '1'
-            work = work.filter { line -> line[i] == filterValue }
-            if (work.size == 1) {
-                break
-            }
-        }
-        val csr = work.first().toInt(2)
-
+        val ogr = part2(::gammaSelector)
+        val csr = part2(::epsilonSelector)
         return ogr * csr
     }
+
+    private fun part2(selector: (List<Char>) -> Char) =
+        filter(report, selector, 0).single().toInt(2)
+
+    private tailrec fun filter(report: List<String>, selector: (List<Char>) -> Char, i: Int): List<String> {
+        if (report.size == 1) {
+            return report
+        }
+        val filterValue = selectChar(report, i, selector)
+        val filtered = report.filter { line -> line[i] == filterValue }
+        return filter(filtered, selector, i + 1)
+    }
+
+    private fun selectChar(report: List<String>, index: Int, selector: (List<Char>) -> Char): Char {
+        val charsAtIndex = report.map { line -> line[index] }
+        return selector(charsAtIndex.sortedByOccurrenceAndValue())
+    }
+
+    private fun List<Char>.sortedByOccurrenceAndValue() =
+        groupingBy { it }.eachCount().toList().sortedWith(compareBy({ it.second }, { it.first })).map { it.first }
 }
