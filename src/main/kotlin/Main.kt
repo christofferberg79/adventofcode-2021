e2cda63289aea154ef63eda9f03b70f9ff319485
Christoffer Berg <christofferberg79@gmail.com>
2021-12-06 10:59:36 +0100

    Format output

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,27 @@
 package cberg.aoc2021
 
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
+
 fun main() {
-    println("Day 1, part 1: ${Day1().part1()}")
-    println("Day 1, part 2: ${Day1().part2()}")
-    println("Day 2, part 1: ${Day2().part1()}")
-    println("Day 2, part 2: ${Day2().part2()}")
-    println("Day 3, part 1: ${Day3().part1()}")
-    println("Day 3, part 2: ${Day3().part2()}")
-    println("Day 4, part 1: ${Day4().part1()}")
-    println("Day 4, part 2: ${Day4().part2()}")
-    println("Day 5, part 1: ${Day5().part1()}")
-    println("Day 5, part 2: ${Day5().part2()}")
-    println("Day 6, part 1: ${Day6().part1(80)}")
-    println("Day 6, part 2: ${Day6().part2(256)}")
+    println("Puzzle           Result               Time")
+    println("------------------------------------------")
+    run(1, 1) { Day1().part1() }
+    run(1, 2) { Day1().part2() }
+    run(2, 1) { Day2().part1() }
+    run(2, 2) { Day2().part2() }
+    run(3, 1) { Day3().part1() }
+    run(3, 2) { Day3().part2() }
+    run(4, 1) { Day4().part1() }
+    run(4, 2) { Day4().part2() }
+    run(5, 1) { Day5().part1() }
+    run(5, 2) { Day5().part2() }
+    run(6, 1) { Day6().part1(80) }
+    run(6, 2) { Day6().part2(256) }
+}
+
+@OptIn(ExperimentalTime::class)
+private fun run(day: Int, part: Int, block: () -> Any) {
+    val result = measureTimedValue(block)
+    println("Day %s, part %s    %-15s %6s ms".format(day, part, result.value, result.duration.inWholeMilliseconds))
 }
