cdce5300accb5eff243d2a0df0a3112f6f1466eb
Christoffer Berg <christoffer.berg@collectum.se>
2021-12-15 15:54:01 +0100

    Optimized Day15
    after reading about Dijkstra's algorithm

--- src/main/kotlin/Day15.kt
+++ src/main/kotlin/Day15.kt
@@ -17,7 +17,7 @@ class Day15(private val input: List<String>) {
         val ySize = input.first().length
         val max = P(xSize * 5 - 1, ySize * 5 - 1)
 
-        val risk: (P) -> Int? = { p:P ->
+        val risk: (P) -> Int? = { p: P ->
             if (p.x < 0 || p.x > max.x || p.y < 0 || p.y > max.y) {
                 null
             } else {
@@ -36,18 +36,20 @@ class Day15(private val input: List<String>) {
 
     private fun P.adjacent() = listOf(P(x, y - 1), P(x, y + 1), P(x - 1, y), P(x + 1, y))
 
+    private data class Node(val p: P, val risk: Int)
+
     private fun findPathRisks(risk: (P) -> Int?): Map<P, Int> {
         val paths = mutableMapOf(P(0, 0) to 0).withDefault { Int.MAX_VALUE }
-        val todo = mutableListOf(P(0, 0))
+        val todo = sortedSetOf(compareBy({ it.risk }, { it.p.x }, { it.p.y }), Node(P(0, 0), 0))
         while (todo.isNotEmpty()) {
-            val p = todo.removeFirst()
-            for (o in p.adjacent()) {
+            val node = todo.pollFirst() ?: error("todo should not be empty")
+            for (o in node.p.adjacent()) {
                 risk(o)?.let { riskAtO ->
                     val oldRisk = paths.getValue(o)
-                    val newRisk = paths.getValue(p) + riskAtO
+                    val newRisk = paths.getValue(node.p) + riskAtO
                     if (newRisk < oldRisk) {
                         paths[o] = newRisk
-                        todo += o
+                        todo += Node(o, newRisk)
                     }
                 }
             }
